25f97457fa43434a7e21feb2ed6eeaf392dc036c
csamatov96 <csamatov96@gmail.com>
2021-01-13 21:04:17 -0600

    vpc,subnet,as,ec2,lb added

--- vpc-lb/resources.tf
+++ vpc-lb/resources.tf
@@ -42,6 +42,17 @@ resource "aws_subnet" "subnet1" {
   }
 }
 
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.vpc.id 
+  cidr_block = var.subnet2_address_space
+  map_public_ip_on_launch = "true"
+  availability_zone = data.aws_availability_zones.available.names[1] #list
+
+  tags = {
+    Name = "subnet2"
+  }
+}
+
 resource "aws_route_table" "rtb" {
   vpc_id = aws_vpc.vpc.id 
 
@@ -60,6 +71,30 @@ resource "aws_route_table_association" "rta-subnet1" {
   route_table_id = aws_route_table.rtb.id
 }
 
+resource "aws_route_table_association" "rta-subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+resource "aws_security_group" "elb-sg" {
+  name        = "nginx_elb_sg"
+  vpc_id      = aws_vpc.vpc.id 
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_security_group" "allow_ssh_http" {
   name        = "sec_group"
   description = "Allow SSH/HTTP traffic"
@@ -75,7 +110,7 @@ resource "aws_security_group" "allow_ssh_http" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.network_address_space]
   }
   egress {
     from_port   = 0
@@ -83,8 +118,24 @@ resource "aws_security_group" "allow_ssh_http" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_elb" "web" {
+  name               = "nginx-elb"
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  security_groups    = [aws_security_group.elb-sg.id]
+  instances          = [aws_instance.nginx1.id, aws_instance.nginx2.id]
+#  availability_zones = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
   tags = {
-    Name = "allow_ssh_http"
+    Environment = "production"
   }
 }
 
@@ -109,12 +160,39 @@ resource "aws_instance" "nginx1" {
     ]
   }
 
-  tags = {
+  tags = { 
     Name = "ec2_vm"
   }
 }
 
+resource "aws_instance" "nginx2" {
+  ami                    = data.aws_ami.aws-linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.subnet2.id 
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip 
+    user        = "ec2-user"
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start",
+    ]
+  }
+
+  tags = { 
+    Name = "ec2_vm"
+  }
+}
+
+
 output "aws_instance_public_dns" {
-  value = aws_instance.nginx1.public_dns
+  value = aws_instance.nginx2.public_dns
 
 }
